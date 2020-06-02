c791e7c62b9ba02bcd6ef404e576df2678bf16b1
ruge.wu <845376854>
2020-06-02 18:20:58 +0800

    web页面init

---
+++ src/main/java/com/ruge/ruge_blog_semantic/RugeBlogSemanticApplication.java
@@ -0,0 +1,13 @@
+package com.ruge.ruge_blog_semantic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RugeBlogSemanticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RugeBlogSemanticApplication.class, args);
+    }
+
+}
